898b77e27fc33961e423d8509e1f03581197db91
dipadana <putudipadana@gmail.com>
2019-12-16 07:52:39 +0700

    update history page

--- src/user/containers/cart/index.jsx
+++ src/user/containers/cart/index.jsx
@@ -1,69 +1,169 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom'
+import Axios from '../../../api/axios'
 import './style.scss'
 
 function Cart() {
+
+  const history = useHistory()
+  const [totalPrice, setTotalPrice]  = useState(0)
+  const [cartData, setCartData] = useState([])
+
+  async function fetchCartData () {
+    try{
+      const { data } = await Axios({
+        method: 'get',
+        url: '/carts',
+        headers: {
+          token: localStorage.getItem('token')
+        }
+      })
+      console.log(data)
+      setCartData(data)
+    }
+    catch(err){
+      console.log(err.response)
+    }
+  }
+
+  async function plusCart (id, qtyNow) {
+    try{
+      const { data } = await Axios({
+        method: 'patch',
+        url: `/carts/${id}/update`,
+        headers: {
+          token: localStorage.getItem('token')
+        },
+        data: {
+          qty: Number(qtyNow) + 1
+        }
+      })
+      console.log(data)
+      fetchCartData()
+    }
+    catch(err){
+      console.log(err.response)
+    }
+  }
+
+  async function minusCart (id, qtyNow) {
+    if(qtyNow === 1){
+      console.log('1')
+    }
+    else{
+      try{
+        const { data } = await Axios({
+          method: 'patch',
+          url: `/carts/${id}/update`,
+          headers: {
+            token: localStorage.getItem('token')
+          },
+          data: {
+            qty: Number(qtyNow) - 1
+          }
+        })
+        console.log(data)
+        fetchCartData()
+      }
+      catch(err){
+        console.log(err.response)
+      }
+    }
+  }
+
+  async function deleteCartData (id) {
+    try{
+      const { data } = await Axios({
+        method: 'delete',
+        url: `/carts/${id}/delete`,
+        headers: {
+          token: localStorage.getItem('token')
+        }
+      })
+      console.log(data)
+      fetchCartData()
+    }
+    catch(err){
+      console.log(err.response)
+    }
+  }
+
+  async function checkout () {
+    try{
+      const { data } = await Axios({
+        method: 'get',
+        url: '/transactions/new',
+        headers: {
+          token: localStorage.getItem('token')
+        }
+      })
+      console.log(data)
+      history.push('/home/history')
+    }
+    catch(err){
+      console.log(err.response)
+    }
+  }
+
+  useEffect(() => {
+    let totalPrice = 0
+    cartData.forEach(data => {
+      totalPrice += Number(data.idBook.price) * Number(data.qty)
+    })
+    setTotalPrice(totalPrice)
+  },[cartData])
+
+  useEffect(() => {
+    fetchCartData()
+  },[])
   
   return (
     <div>
       <div className="container">
-        <p className="cart-title">Showing <b>2 product</b> in cart</p>
+        <p className="cart-title">Showing <b>{cartData.length} product</b> in cart</p>
         <div className="row">
           <div className="col-9">
             <div className="cartContainer">
 
-              <div className="row cartContainer--cartItem row no-gutters">
-                <div className="col-2">
-                  <img alt="book cover" className="cartContainer--cartItem--image" src="https://cdn.gramedia.com/uploads/items/9786020635163_MANAJEMEN_RISIKO_KRISIS_DAN_BENCANA_UNTUK_INDUSTRI_PARIWISATA_YANG_BERKELANJUTAN-1__w110_hauto.jpg" />
-                </div>
-                <div className="col-5">
-                  <p className="mb-0 cartContainer--cartItem--book-title">Manajemen Risiko, Krisis, Dan Bencana Untuk Industri Pariwisata yang Berkelanjutan</p>
-                  <p className="cartContainer--cartItem--author"> <small>DIPADANA PUTU</small></p>
-                  <p><b>Rp 20.000</b></p>
-                </div>
-                <div className="col-2">
-                  <div className="d-flex justify-content-end">
-                    <button className="cartContainer--cartItem--plus-minus-btn">-</button>
-                    <input className="cartContainer--cartItem--number-input" value="0" type="number"/>
-                    <button className="cartContainer--cartItem--plus-minus-btn">+</button>
+              {
+                cartData.length === 0
+                ?
+                <p>No Data</p>
+                :
+                cartData.map((data,i) => 
+                  <div key={i} className="row cartContainer--cartItem row no-gutters">
+                    <div className="col-2">
+                      <img alt="book cover" className="cartContainer--cartItem--image" src={data.idBook.image} />
+                    </div>
+                    <div className="col-5">
+                      <p className="mb-0 cartContainer--cartItem--book-title">{data.idBook.title}</p>
+                      <p className="cartContainer--cartItem--author"> <small>{data.idBook.author[0]}</small></p>
+                      <p><b>Rp {data.idBook.price}</b></p>
+                    </div>
+                    <div className="col-2">
+                      <div className="d-flex justify-content-end">
+                        <button onClick={ () => minusCart(data._id, data.qty) } className="cartContainer--cartItem--plus-minus-btn">-</button>
+                        <input readOnly min="1" className="cartContainer--cartItem--number-input" value={data.qty} type="number"/>
+                        <button onClick={ () => plusCart(data._id, data.qty) } className="cartContainer--cartItem--plus-minus-btn">+</button>
+                      </div>
+                    </div>
+                    <div className="col-2">
+                      <p className="cartContainer--cartItem--subtotal-text">Subtotal</p>
+                      <p className="cartContainer--cartItem--subtotal-number">Rp {Number(data.idBook.price) * Number(data.qty)}</p>
+                      <p onClick={ () => deleteCartData(data._id) } className="cartContainer--cartItem--delete">DELETE</p>
+                    </div>
+                    <div className="offset-1"></div>
                   </div>
-                </div>
-                <div className="col-2">
-                  <p className="cartContainer--cartItem--subtotal-text">Subtotal</p>
-                  <p className="cartContainer--cartItem--subtotal-number">Rp 40.000</p>
-                </div>
-                <div className="offset-1"></div>
-              </div>
-
-              <div className="row cartContainer--cartItem row no-gutters">
-                <div className="col-2">
-                  <img alt="book cover" className="cartContainer--cartItem--image" src="https://cdn.gramedia.com/uploads/items/9786020635163_MANAJEMEN_RISIKO_KRISIS_DAN_BENCANA_UNTUK_INDUSTRI_PARIWISATA_YANG_BERKELANJUTAN-1__w110_hauto.jpg" />
-                </div>
-                <div className="col-5">
-                  <p className="mb-0 cartContainer--cartItem--book-title">Manajemen Risiko, Krisis, Dan Bencana Untuk Industri Pariwisata yang Berkelanjutan</p>
-                  <p className="cartContainer--cartItem--author"> <small>DIPADANA PUTU</small></p>
-                  <p><b>Rp 20.000</b></p>
-                </div>
-                <div className="col-2">
-                  <div className="d-flex justify-content-end">
-                    <button className="cartContainer--cartItem--plus-minus-btn">-</button>
-                    <input className="cartContainer--cartItem--number-input" value="0" type="number"/>
-                    <button className="cartContainer--cartItem--plus-minus-btn">+</button>
-                  </div>
-                </div>
-                <div className="col-2">
-                  <p className="cartContainer--cartItem--subtotal-text">Subtotal</p>
-                  <p className="cartContainer--cartItem--subtotal-number">Rp 40.000</p>
-                </div>
-                <div className="offset-1"></div>
-              </div>
+                )
+              }
 
             </div>
           </div>
           <div className="col-3">
             <div className="checkout-container">
               <p className="checkout-container--title">Total Price</p>
-              <p className="checkout-container--total-price"><b>Rp 40.000</b></p>
-              <button className="checkout-container--button">Checkout</button>
+              <p className="checkout-container--total-price"><b>Rp {totalPrice}</b></p>
+              <button onClick={ () => checkout() } className="checkout-container--button">Checkout</button>
             </div>
           </div>
         </div>
--- src/user/containers/history/index.jsx
+++ src/user/containers/history/index.jsx
@@ -1,31 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from '../../../api/axios'
+import Moment from 'moment'
 import './style.scss'
 
 export default function History (props) {
+
+  const [historyData, setHistoryData] = useState([])
+
+  async function fetchHistoryData () {
+    try{
+      const {data} = await Axios({
+        method: 'get',
+        url: '/transactions/user',
+        headers: {
+          token: localStorage.getItem('token')
+        }
+      })
+      console.log(data)
+
+      data.forEach(data => {
+        let tempFinal = []
+        let totalPrice = {
+          qty: 0,
+          price: 0
+        }
+        data.cart.forEach(data => {
+          tempFinal.push({qty:data.qty, price:data.bookId.price})
+        })
+        tempFinal.forEach(data => {
+          totalPrice.price += data.price
+          totalPrice.qty += data.qty
+        })
+        data.totalTransactions = totalPrice
+      })
+      console.log(data)
+      setHistoryData(data)
+    }
+    catch(err){
+      console.log(err.response)
+    }
+  }
+
+  useEffect(() => {
+    fetchHistoryData()
+  },[])
+
   return (
     <div>
       <p className="transaction-main-title">Transaction History</p>
-      <div className="transaction-container">
-        <div className="row transaction-container--item">
-          <div className="col-3">
-            <p className="mb-0 transaction-container--title">Date</p>
-            <p className="mb-0 transaction-container--text">January, 15th 2019</p>
+
+      {
+        historyData.length === 0
+        ?
+        <p>No Data</p>
+        :
+        historyData.map((data,i) => 
+          <div className="transaction-container">
+            <div className="row transaction-container--item">
+              <div className="col-3">
+                <p className="mb-0 transaction-container--title">Date</p>
+                <p className="mb-0 transaction-container--text">{Moment(String(new Date(data.createdAt))).format("MMMM Do YYYY")}</p>
+              </div>
+              <div className="col-3">
+                <p className="mb-0 transaction-container--title">Order ID</p>
+                <p className="mb-0 transaction-container--text">{data._id}</p>
+              </div>
+              <div className="col-3">
+                <p className="mb-0 transaction-container--title">Total payment</p>
+                <p className="mb-0 transaction-container--text">Rp {data.totalTransactions.price}</p>
+              </div>
+              <div className="col-3">
+                <p className="mb-0 transaction-container--title">Status</p>
+                <p className="mb-0 transaction-container--text">Transaction Done</p>
+              </div>
+            </div>
           </div>
-          <div className="col-3">
-            <p className="mb-0 transaction-container--title">Order ID</p>
-            <p className="mb-0 transaction-container--text">aja9qnknndna299128nnd</p>
-          </div>
-          <div className="col-3">
-            <p className="mb-0 transaction-container--title">Total payment</p>
-            <p className="mb-0 transaction-container--text">Rp 10.000</p>
-          </div>
-          <div className="col-3">
-            <p className="mb-0 transaction-container--title">Status</p>
-            <p className="mb-0 transaction-container--text">Transaction Done</p>
-          </div>
-        </div>
-      </div>
+        )
+      }
       
+
     </div>
   )
 }
