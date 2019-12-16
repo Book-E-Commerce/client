aaa72be76925805e6c5c2a01aa553656193c13b0
ashirogisen <m.rizki.aiman@gmail.com>
2019-12-16 15:22:16 +0700

    Details page updated

--- src/user/containers/register/index.jsx
+++ src/user/containers/register/index.jsx
@@ -10,7 +10,8 @@ function Register() {
   const [ password, setPassword ] = useState('')
   const [ username, setUsername ] = useState('')
 
-  const registerUser = () => {
+  const registerUser = (e) => {
+    e.preventDefault()
     axios.post('/users/register', {
       email, password, username
     })
@@ -34,22 +35,24 @@ function Register() {
           <div className="register-container--right--registerbox--header">
             <img src="https://i.imgur.com/rIDjqeJ.png" alt="Icon"/>
           </div>
-          <div className="form-group register-container--right--registerbox--form">
-            <label for="exampleInputEmail1">Full Name</label>
-            <input value={username} onChange={event => setUsername(event.target.value)} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          </div>
-          <div className="form-group register-container--right--registerbox--form">
-            <label for="exampleInputEmail1">Email address</label>
-            <input value={email} onChange={event => setEmail(event.target.value)} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <small id="emailHelp" className="form-text text-muted">Please input verified email.</small>
-          </div>
-          <div style={{marginBottom: '15px'}} className="form-group register-container--right--registerbox--form">
-            <label for="exampleInputPassword1">Password</label>
-            <input value={password} onChange={event => setPassword(event.target.value)} type="password" className="form-control" id="exampleInputPassword1" />
-          </div>
-          <div className="register-container--right--registerbox--register-btn-container">
-            <button onClick={registerUser} type="submit" className="btn register-container--right--registerbox--register-btn">Register</button>
-          </div>  
+          <form onSubmit={registerUser}>
+            <div className="form-group register-container--right--registerbox--form">
+              <label for="exampleInputEmail1">Full Name</label>
+              <input value={username} onChange={event => setUsername(event.target.value)} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            </div>
+            <div className="form-group register-container--right--registerbox--form">
+              <label for="exampleInputEmail1">Email address</label>
+              <input value={email} onChange={event => setEmail(event.target.value)} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <small id="emailHelp" className="form-text text-muted">Please input verified email.</small>
+            </div>
+            <div style={{marginBottom: '15px'}} className="form-group register-container--right--registerbox--form">
+              <label for="exampleInputPassword1">Password</label>
+              <input value={password} onChange={event => setPassword(event.target.value)} type="password" className="form-control" id="exampleInputPassword1" />
+            </div>
+            <div className="register-container--right--registerbox--register-btn-container">
+              <button onClick={registerUser} type="submit" className="btn register-container--right--registerbox--register-btn">Register</button>
+            </div>  
+          </form>
           <div className="register-container--right--registerbox--login-btn-container">
             <Link to ="/login">
               <button type="submit" className="btn register-container--right--registerbox--login-btn">Login</button>
